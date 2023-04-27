d47bd12c87faf61e0806f3a0f1f0bd4fdcdd9b1e
jnatmed <49349742+jnatmed@users.noreply.github.com>
2023-04-27 09:40:03 -0300

    formulario controlado

--- gestor.js
+++ gestor.js
@@ -1,3 +1,5 @@
+import Toastify from 'toastify-js';
+import "toastify-js/src/toastify.css";
 export class Gestor{
     $(idElement){
         return document.querySelector(idElement);
@@ -8,4 +10,16 @@ export class Gestor{
     consola(msj){
         console.log(msj)
     }
+    mostrarCartel(cartel, duracion = 5000){
+        Toastify({
+            text: cartel,
+            duration: duracion,
+            className: "info",
+            gravity: "bottom", // `top` or `bottom`
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
+        }).showToast();
+    };
+
 }
--- main.js
+++ main.js
@@ -2,28 +2,35 @@ import { Gestor } from "./gestor";
 
 let gestor = new Gestor()
 let input = gestor.$('#enviar')
+let paises = {"argentina" : 50, "uruguay" : 60, "chile" : 70}
 
 input.addEventListener('click', e =>{
   e.preventDefault();
-
+  let formularioValido = true;
   ['#nombre','#apellido','#edad','#evento','#pais','#cantidad'].forEach(input => {
       let campo = input.split('#')[1];
       if(gestor.$(input).value==""){
-        console.log(`EL CAMPO "${(campo).toLocaleUpperCase()}" ESTA VACIO`)
+        gestor.mostrarCartel(`EL CAMPO "${(campo).toLocaleUpperCase()}" ESTA VACIO`)
+        formularioValido = false;
       }
       
       if(campo=="edad" && gestor.$(input).value<18){
-        console.log(`EL CAMPO ${(campo).toLocaleUpperCase()} DEBE SER MAYOR A 18`)
+        gestor.mostrarCartel(`EL CAMPO ${(campo).toLocaleUpperCase()} DEBE SER MAYOR A 18`)
+        formularioValido = false;
+      }    
+
+      if(campo=="cantidad" && gestor.$(input).value > paises[gestor.$('#pais').value]){
+        gestor.mostrarCartel(`Cantidad no permitida para ${gestor.$('#pais').value}`)
+        formularioValido = false;
       }
-
-      let paises = {"argentina" : 50, "uruguay" : 60, "chile" : 70}    
-
-      if(campo=="cantidad" && gestor.$(input).value < paises[gestor.$('#pais').value]){
-        console.log(`Cantidad no permitida para ${gestor.$('#pais').value}`)
-      }
-
+  // })
+    
   });
-
+  if(formularioValido){
+    gestor.mostrarCartel('FORMULARIO VALIDADO, felicitaciones')
+  }else{
+    gestor.mostrarCartel('FORMULARIO NO COMPLETO O LLENADO DE MANERA INCORRECTA')
+  }
   
 })
 
